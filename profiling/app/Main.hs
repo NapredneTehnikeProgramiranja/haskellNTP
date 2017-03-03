0e011cdb4c74e8e695fdef19525a8dc1a69b31dd
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-03 15:55:24 +0100

    Simple profiling

--- profiling/app/Main.hs
+++ profiling/app/Main.hs
@@ -1,6 +1,18 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Main where
 
-import Lib
+--------------------------------------------------------------------------------
+-- Simple profiling
 
 main :: IO ()
-main = someFunc
+main = print (f 30 + g 30)
+  where
+    f n = fib n
+    g n = fib (n `div` 2)
+
+
+fib :: (Num t, Num a, Eq a) => a -> t
+fib 0 = 1
+fib 1 = 0
+fib n = fib (n - 1) + fib (n - 2)
