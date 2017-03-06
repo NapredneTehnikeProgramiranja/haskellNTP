69c7b0ea2c6c2ed4a62ecef333051e5bd2e4e0f1
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-06 16:02:21 +0100

    hp2ps

--- profiling/src/ExplicitCostCenters.hs
+++ profiling/src/ExplicitCostCenters.hs
@@ -2,6 +2,7 @@ module ExplicitCostCenters
   ( explicitCostCenters
   ) where
 
+explicitCostCenters :: IO ()
 explicitCostCenters = do print $ {-# SCC last_xs #-} last xs
                          print $ {-# SCC last_init_xs #-} last $ init xs
                          print $ {-# SCC last_ys #-} last ys
